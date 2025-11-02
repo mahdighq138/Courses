b515d14f58492ea8d2622c5a55991abf90cb856a
mahdi ghasri <mmgh001600@gmail.com>
2025-11-02 11:32:26 +0330

    Feature: Add Sign In and Sign Up DTOs/ViewModels

---
+++ Courses.Application/DTOs/AccountViewModels/SignInViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Application.DTOs.AccountViewModels
+{
+    public class SignInViewModel
+    {
+        public required string UserNameOrEmail { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ Courses.Application/DTOs/AccountViewModels/SignUpViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Application.DTOs.AccountViewModels
+{
+    public class SignUpViewModel
+    {
+        [MaxLength(50, ErrorMessage = "First Name Too Long")]
+        public string? FirstName { get; set; }
+
+
+        [MaxLength(50, ErrorMessage = "Last Name Too Long")]
+        public string? LastName { get; set; }
+
+
+        [MaxLength(50, ErrorMessage = "User Name Too Long")]
+        public required string UserName { get; set; }
+
+
+        [MaxLength(100, ErrorMessage ="Email Address Too Long")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public required string Email { get; set; }
+
+
+        public required string Password { get; set; }
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public required string ConfirmPassword { get; set; }
+    }
+}
