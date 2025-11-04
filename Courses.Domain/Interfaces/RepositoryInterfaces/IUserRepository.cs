691533d75c2f47eb6ebee9e15b28223ed5b16524
mahdi ghasri <mmgh001600@gmail.com>
2025-11-04 09:42:58 +0330

    Feature: add IPasswordHasher and IUserRepository interfaces

---
+++ Courses.Domain/Interfaces/RepositoryInterfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Courses.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Domain.Interfaces.RepositoryInterfaces
+{
+    public interface IUserRepository
+    {
+        public Task<bool> EmailExistsAsync(string email);
+        public Task<bool> UserNameExistsAsync(string username);
+        public Task<int> RegisterUserAsync(User user);
+    }
+}
---
+++ Courses.Domain/Interfaces/SecurityInterfaces/IPasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Domain.Interfaces.SecurityInterfaces
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string hashedPassword, string providedPassword);
+    }
+}
