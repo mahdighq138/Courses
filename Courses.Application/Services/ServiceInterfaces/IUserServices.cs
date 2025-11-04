e44a2ae764dcdcc1b725b85aaf0e3f258cabc4ca
mahdi ghasri <mmgh001600@gmail.com>
2025-11-04 09:46:56 +0330

    Feature: add UserServices interface and implementation

---
+++ Courses.Application/Services/ServiceInterfaces/IUserServices.cs
@@ -0,0 +1,16 @@
+﻿using Courses.Application.DTOs.AccountViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Application.Services.ServiceInterfaces
+{
+    public interface IUserServices
+    {
+        Task<int> RegisterUserAsync(SignUpViewModel signUpViewModel);
+        public Task<bool> EmailExistsAsync(string email);
+        public Task<bool> UserNameExistsAsync(string username);
+    }
+}
---
+++ Courses.Application/Services/UserService/UserServices.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Courses.Application.DTOs.AccountViewModels;
+using Courses.Application.Services.ServiceInterfaces;
+using Courses.Domain.Convertors;
+using Courses.Domain.Entities.User;
+using Courses.Domain.Generator;
+using Courses.Domain.Interfaces.RepositoryInterfaces;
+using Courses.Domain.Interfaces.SecurityInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Application.Services.UserService
+{
+    public class UserServices(IMapper mapper, IPasswordHasher hasher, IUserRepository userRepository) : IUserServices
+    {
+        private readonly IMapper _mapper = mapper;
+        private readonly IPasswordHasher _hasher = hasher;
+        private readonly IUserRepository _userRepository = userRepository;
+
+ 
+        public async Task<int> RegisterUserAsync(SignUpViewModel signUpViewModel)
+        {
+            signUpViewModel.Email = FixText.FixEmail(signUpViewModel.Email);
+
+            
+
+            var userEntity = _mapper.Map<User>(signUpViewModel);
+            userEntity.RegistrationDate = DateTime.Now;
+            userEntity.ActivationCode = CodeNameGenerator.GenerateUniqueCode();
+            userEntity.IsActive = false;
+            userEntity.Password = _hasher.HashPassword(userEntity.Password);
+            return await _userRepository.RegisterUserAsync(userEntity);
+
+        }
+
+        public Task<bool> EmailExistsAsync(string email)
+        {
+            return _userRepository.EmailExistsAsync(email);
+        }
+
+
+        public Task<bool> UserNameExistsAsync(string username)
+        {
+            return _userRepository.UserNameExistsAsync(username);
+        }
+    }
+}
