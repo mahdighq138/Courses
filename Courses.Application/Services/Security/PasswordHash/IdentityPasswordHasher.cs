8943cc00e24e5dbfe455d0cf3b69ca54099b08f7
mahdi ghasri <mmgh001600@gmail.com>
2025-11-04 09:45:26 +0330

    Feature: add Identity Password Hasher

---
+++ Courses.Application/Services/Security/PasswordHash/IdentityPasswordHasher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Courses.Domain.Entities.User;
+using Courses.Domain.Interfaces.SecurityInterfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Identity;
+
+namespace Courses.Application.Services.Security.PasswordHash
+{
+    public class IdentityPasswordHasher : IPasswordHasher
+    {
+        private readonly PasswordHasher<object> _hasher = new();
+
+        public string HashPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentNullException();
+            }
+            return _hasher.HashPassword(null, password);
+
+        }
+
+        public bool VerifyPassword(string hashedPassword, string providedPassword)
+        {
+            if (string.IsNullOrEmpty(hashedPassword) || string.IsNullOrEmpty(providedPassword))
+            {
+                throw new ArgumentNullException();
+            }
+            var result = _hasher.VerifyHashedPassword(null, hashedPassword, providedPassword);
+            return result != PasswordVerificationResult.Failed;
+            
+        }
+    }
+}
