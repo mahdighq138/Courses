d876db1524e6f1dc4a6960c3995549527b041662
mahdi ghasri <mmgh001600@gmail.com>
2025-11-04 09:38:32 +0330

    Feature: add Trim and ToLower for emails

---
+++ Courses.Domain/Convertors/FixText.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Domain.Convertors
+{
+    public static class FixText
+    {
+        public static string FixEmail(string email)
+        {
+            return email.Trim().ToLower();
+        }
+    }
+}
