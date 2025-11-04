22632c3cda1f541fcb86885717fa83159b7d3e01
mahdi ghasri <mmgh001600@gmail.com>
2025-11-04 10:44:32 +0330

    Fix: Add rememberMe Field and MaxLength Attribute for UserNameOrEmail

--- Courses.Application/DTOs/AccountViewModels/SignInViewModel.cs
+++ Courses.Application/DTOs/AccountViewModels/SignInViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace Courses.Application.DTOs.AccountViewModels
 {
     public class SignInViewModel
     {
+        [MaxLength(100, ErrorMessage = "Email / UserName Too Long")]
         public required string UserNameOrEmail { get; set; }
         public required string Password { get; set; }
+        public bool RememberMe { get; set; }
     }
 }
