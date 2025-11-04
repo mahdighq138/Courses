1c8d5f27ddf9767854490a8075bf48a82a6c26ac
mahdi ghasri <mmgh001600@gmail.com>
2025-11-04 09:34:20 +0330

    Feature: add code generator as activationCode

---
+++ Courses.Domain/Generator/CodeNameGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Domain.Generator
+{
+    public class CodeNameGenerator
+    {
+        public static string GenerateUniqueCode()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
+        }
+    }
+}
