a134d6b498739877b50956841cb8556cebcff7ee
mahdi ghasri <mmgh001600@gmail.com>
2025-11-04 09:51:12 +0330

    Feature: add DI for Password Hasher, AutoMapper, User service and repository

--- Courses.WebApp/Program.cs
+++ Courses.WebApp/Program.cs
@@ -1,10 +1,26 @@
+using Courses.Application.Mappers.UserMapper;
 using Courses.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Courses.Domain.Interfaces.SecurityInterfaces;
+using Courses.Application.Services.Security.PasswordHash;
+using Courses.Application.Services.ServiceInterfaces;
+using Courses.Application.Services.UserService;
+using Courses.Domain.Interfaces.RepositoryInterfaces;
+using Courses.Infrastructure.Repositories.UserRepo;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+#region Add UserRepository
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+#endregion
+
+#region Add UserService
+builder.Services.AddScoped<IUserServices, UserServices>();
+#endregion
 
 #region Add DbContext
 builder.Services.AddDbContext<CoursesDbContext>(options =>
@@ -13,6 +29,16 @@ builder.Services.AddDbContext<CoursesDbContext>(options =>
 });
 #endregion
 
+#region Add AutoMapper
+builder.Services.AddAutoMapper(cfg =>
+{
+    
+}, typeof(UserMappingProfile).Assembly);
+#endregion
+
+#region Add PasswordHasher
+builder.Services.AddTransient<IPasswordHasher, IdentityPasswordHasher>();
+#endregion
 
 var app = builder.Build();
 
